84919c548122d861b6398c07ef745069fab71bdd
Marco Giammari <marcogiammari@gmail.com>
2023-03-31 13:33:36 +0200

    add pie chart

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ['Never', 'Every few hours', 'Every hour', 'Multiple times per hour', 'Constantly'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
